1a1020ae43e5ac08c56fc8e7493db8fb8e3a26e0
gnickel <gnickel@student.42heilbronn.de>
2024-12-09 12:50:58 +0100

    Restarted and added/changed libft utils

--- get_next_line.c
+++ get_next_line.c
@@ -12,30 +12,6 @@
 
 #include "get_next_line.h"
 
-int	newline_check(t_buffer *head)
-{
-	while (head)
-	{
-		if (ft_strchr(head->buffer, '\n'))
-			return (1);
-		head = head->next;
-	}
-	return (0);
-}
-
 char	*get_next_line(int fd)
 {
-	static t_buffer	*head;
-	char			tmp[BUFFER_SIZE + 1];
-	char			*line;
-	int				b_read;
-
-	while (!newline_check(head))
-	{
-		b_read = read(fd, tmp, BUFFER_SIZE);
-		if (b_read <= 0)
-			break ;
-		tmp[b_read] = '\0';
-	}
-	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -12,19 +12,20 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+#endif
 
 #include "unistd.h"
-
-typedef struct s_buffer
-{
-	char			*buffer;
-	struct s_buffer	*next;
-}	t_buffer;
+#include "stdlib.h"
 
 char	*get_next_line(int fd);
 
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
